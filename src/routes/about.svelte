cf36f429aae8d8057457fcde449b0969cecafe1c
Kelvin Takyi-Bobi <52892093+KELs7@users.noreply.github.com>
2022-07-26 01:52:17 +0000

    works! data is pulled from lamden block service

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,53 +1,46 @@
-<!-- <script context='module'>
+<script context='module'>
     export async function load({fetch}){
         const res = await fetch('http://localhost:3535/current/all/multi_sign/');
-        const d = await await res.json();
-
+        const blockServiceData = await res.json();
         if(res.ok){
             return {
-                props: {d}
+                props: {blockServiceData}
             }
         }
     }
-</script> -->
+</script> 
 
 <script>
     import CollapsableBar from '$lib/collapsableBar.svelte';
-    
-    let owners = ['eeddababababababababababababababababababababababababababababcdff','abababababababababababababababababababababababababababababababab']
-    owners.forEach((address)=>{
-        let index = owners.indexOf(address);
+
+    export let blockServiceData;
+
+    const ownerList = blockServiceData.multi_sign.owners
+    ownerList.forEach((address)=>{
+        let index = ownerList.indexOf(address);
         let shortenedAddress = address.substring(0,7) + ' . . . ' + address.substring(60,64)
-        owners[index] = shortenedAddress
+        ownerList[index] = shortenedAddress
     })
-    export let d ={
-        
-        owners: owners, 
-        required: 2,
+
+    const d ={
+        owners: ownerList, 
+        required: blockServiceData.multi_sign.required,
+        //hardcoded because there's no initial registered action by multisig contract
         actions: {"actionAsset":"con_action"},
-        daily_limit :{"currency": 1000}
+        daily_limit: blockServiceData.multi_sign.daily_limit
     }
 
-    let proposalBarNames = [
+    const proposalBarNames = [
         'Owners',
         'Registered actions',
         'Daily limit(s)',
         'Req. confirms'
     ];
 
-   owners = d.owners;
-    // owners = d.multi_sign.owners
+    const owners = d.owners;
     
-    
-    //let requiredConfirmations = d.multi_sign.required;
     let requiredConfirmations = d.required;
 
-    // let registeredActions = [];
-    // if (d.multi_sign.actions){
-    //     for (const i in d.multi_sign.actions){
-    //         registeredActions.push({action:i, contract:d.multi_sign.actions[i]})
-    //     }
-    // }
     let registeredActions = [];
     if (d.actions){
         for (const i in d.actions){
@@ -55,22 +48,12 @@
         }
     }
 
-
-    // let dailyLimits = [];
-    // if (d.multi_sign.daily_limit){
-    //     for (const i in d.multi_sign.daily_limit){
-    //         dailyLimits.push({token:i, amount:d.multi_sign.daily_limit[i]})
-    //     }
-    // }
-    
     let dailyLimits = [];
     if (d.daily_limit){
         for (const i in d.daily_limit){
-            console.log(d.daily_limit[i])
             dailyLimits.push({token:i, amount:d.daily_limit[i]})
         }
     }
-
 
 </script>
 
@@ -91,7 +74,7 @@
             <div class='actions'>
                 <div style="grid-column: 1/2; color: var(--primary-color)">action</div>
                 <div style="grid-column: 2/4; color: var(--primary-color)">contract</div>
-                {#if d.action}
+                {#if d.actions}
                     {#each registeredActions as actionList}
                         <div style="grid-column: 1/2;">{actionList.action}</div>
                         <div style="grid-column: 2/4;">{actionList.contract}</div>
@@ -107,7 +90,7 @@
                 {#if d.daily_limit}
                     {#each dailyLimits as limitList}
                         <div style="grid-column: 1/2;">{limitList.token}</div>
-                        <div style="grid-column: 2/4;">{limitList.amount}</div>
+                        <div style="grid-column: 2/4;">{limitList.amount.__fixed__}</div>
                     {/each}
                 {/if}
                     
@@ -167,7 +150,4 @@
 		color: #ffffff;
 		font-weight: bold;
 	}
-    /* li {
-        text-align: center;
-    } */
 </style>
