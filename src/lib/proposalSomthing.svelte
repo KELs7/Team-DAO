7a3dfa74301134fc44836d89f8b88a4ef1b55b63
Kelvin Takyi-Bobi <52892093+KELs7@users.noreply.github.com>
2022-07-26 01:47:17 +0000

    proposal infos simultaneous appearance fix

--- src/lib/proposalSomthing.svelte
+++ src/lib/proposalSomthing.svelte
@@ -7,24 +7,18 @@
     const dispatch = createEventDispatcher();
 
     export let confirmed = false;
-    //export let confirmNum = 0;
-    //export let proposalSubmitted = '';
-    //export let proposalType = '';
     export let proposalId = '';
-
     export let proposals = '';
 
-    let confirmBtnLabel = `confirm ${proposals.confirmNum}`
-    let view;
+    let view = 0;
     let viewStateBtn = 'view';
     let revokeBtnLabel = 'revoke';
     let resultMessage = '';
-	let errorInfo = '';
-  	let link = '';
+    let errorInfo = '';
+    let link = '';
     let cBtn;
     let rBtn;
-
-    //console.log(`begin: ${confirmNum}`)
+    //let proposalView;
 
     const confirmBtn = async ()=> {
 
@@ -96,76 +90,37 @@
 </script>
 
 <div class='container'>
-    <!--div class='inside-container'>
-        <div>{proposalId}</div>
-        <div class='ptype'>{proposalType}</div>
-        <div class='action-btn'>
 
-            <button on:click={()=>{
-                view?viewStateBtn='view':viewStateBtn='close';
-                view=!view;
-                }}
-            >{viewStateBtn}</button>
-
-            <Button 
-                on:output={handleOutput}
-                btnLabel='view'
-                returnBtnLabel='view'
-                error=''
-                awaitMode=true
-                fn={confirmBtn}/>
-
-            <button on:click|once={confirmBtn} bind:this={cBtn}>{confirmBtnLabel}</button>
-            
-            <Button 
-                on:output={handleOutput}
-                btnLabel='confirm'
-                returnBtnLabel='confirm'
-                error=''
-                awaitMode=true
-                fn={confirmBtn}/>
-            
-            {#if confirmed}
-                <button on:click|once={revokeBtn} bind:this={rBtn}>{revokeBtnLabel}</button>
-                <Button 
-                    on:output={handleOutput}
-                    btnLabel='revoke'
-                    returnBtnLabel='revoke'
-                    error=''
-                    awaitMode=true
-                    fn={revokeBtn}/>
-            
-            {/if} 
-
-        </div> 
-    </div-->
     <ul class="inside-container">
+
         {#each proposals as proposal}
+
         <li>
             <div style="display: flex; justify-content:space-around; align-content: center;">
                 <div>{proposal.id}</div>
                 <div class='ptype'>{proposal.type}</div>
                 <div class='action-btn'>
 
-                    <!--button on:click={()=>{
-                        view?viewStateBtn='view':viewStateBtn='close';
-                        view=!view;
+                    <button 
+                        class="outlined primary white"
+                        id = {proposal.id}
+                        on:click={()=>{
+                            view==proposal.id?viewStateBtn='view':viewStateBtn='close';
+                            if(view==proposal.id){
+                                view=0;
+                            }else{
+                                view=proposal.id;
+                            }
                         }}
-                    >{viewStateBtn}</button-->
+                    >{viewStateBtn}</button>
 
-                    <Button 
-                        on:output={handleOutput}
-                        btnLabel='view'
-                        returnBtnLabel='view'
-                        error=''
-                        awaitMode=true
-                        fn={confirmBtn}/>
+                    
 
                     <!--button on:click|once={confirmBtn} bind:this={cBtn}>{confirmBtnLabel}</button-->
                     
                     <Button 
                         on:output={handleOutput}
-                        btnLabel='confirm'
+                        btnLabel={`confirm ${proposal.confirmNum}`}
                         returnBtnLabel='confirm'
                         error=''
                         awaitMode=true
@@ -185,11 +140,14 @@
                 </div>        
             </div> 
         </li>
-        {#if view}
-            <div transition:fly='{{y:-20, duration: 900}}' class='proposal'>
+        {#if view == proposal.id}
+            <div transition:fly='{{y:-5, duration: 600}}' class='proposal proposal-background'>
                 {proposal.proposal}
             </div>
         {/if}
+        <!--div bind:this={proposalView} class="proposal-view">
+            {proposal.proposal}
+        </div-->
         {/each}
     </ul>
 </div>
@@ -199,11 +157,9 @@
 <style>
     .container {
         width: 81%;
-        /* height: 3rem; */
         margin: auto;
         margin-top: 0.1rem;
         overflow-x: scroll;
-        /* padding: 1rem 0; */
     }
     .inside-container {
         display: flex;
@@ -213,13 +169,10 @@
         width: 100%;
         box-sizing: border-box;
         margin: auto;
-        /* margin-top: 0.1rem; */
         font-size: 0.7rem;
         color: white;
         background-color: var(--panel-background-color);
         padding: 0.8rem 0 0 0;
-        
-
     }
     .proposal {
         width: 80%;
@@ -232,20 +185,26 @@
         border: 2px solid var(--flat-primary);
 
     }
-
-    /* button {
-        font-size: 0.5rem;
-        font-weight: bold;
-        color: white;
-        border-width: 2px;
-        border-radius: 5px;
-        background-image: var(--gradient-secondary);
-    } */
     li {
         list-style: none;
         padding: 0.8rem 0 0.8rem 0;
         border-bottom: 1px solid #000000;
     }
+    .proposal-background {
+        background-color: #000000;
+        margin: 0;
+        padding: 0.5rem;
+        width: 100%;
+        box-sizing: border-box;
+    }
+    /* .proposal-view {
+        overflow: hidden; 
+        background-color: #000000; 
+        width: 100%;
+        box-sizing: border-box;
+		max-height: 0; 
+		transition: max-height 0.7s ease-out; 
+    } */
     @media (max-width: 480px) {
         .inside-container{
             width: 100vw; 
