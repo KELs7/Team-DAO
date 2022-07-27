77d67a356e7c32dfcd8a0f5466f95b57d7b47b44
kels7 <hiketabo@gmail.com>
2022-07-27 22:02:58 +0000

    pull propsls from blockservice and update table

--- src/routes/pendingProposals.svelte
+++ src/routes/pendingProposals.svelte
@@ -1,21 +1,15 @@
-<!-- <script context='module'>
+<script context='module'>
 	export async function load({fetch}){
-		const [propsl, confirms] = await Promise.all([
-			fetch('http://localhost:3535/current/all/multi_sign/proposal'),
-			fetch('http://localhost:3737/contracts/multi_sign/owner_confirmed?key=1')
-			//fetch('http://localhost:3535/current/all/multi_sign/owner_confirmed')
-		])
+        const res = await fetch('http://localhost:3535/current/all/multi_sign/');
+        const blockServiceData = await res.json();
+        if(res.ok){
+            return {
+                props: {blockServiceData}
+            }
+        }
+    }
 		
-		const p = await propsl.json()
-		const c = await confirms.json()
-
-		
-		return {
-			props: {p, c}
-		};
-		
-	}
-</script> -->
+</script> 
 
 <script>
 	import ProposalList from '$lib/proposalSomthing.svelte';
@@ -24,57 +18,40 @@
     //import { Socket } from 'dgram';
 
 	//import { io } from 'node_modules/socket.io/client-dist/socket.io.js';
-	import { io } from 'socket.io-client';
+	// import { io } from 'socket.io-client';
 
-	const socket = io("ws://localhost:3535");
-	socket.on("connect", ()=>{
-		socket.emit('join','new-state-changes-by-transaction');
-	})
-	socket.on('new-state-changes-by-transaction', (msg)=>{
-		console.log(msg);
-	})
+	// const socket = io("ws://localhost:3535");
+	// socket.on("connect", ()=>{
+	// 	socket.emit('join','new-state-changes-by-transaction');
+	// })
+	// socket.on('new-state-changes-by-transaction', (msg)=>{
+	// 	console.log(msg);
+	// })
 
-	export let p = {
-		proposal: {
-			1:{add_owner: 'dog', type: 'state_update'}, 
-			2:{add_owner: 'dog', type: 'state_update'}
-		}};
+	export let blockServiceData;
+	const noData =false;
 
-	export let c = {value: [$walletAddress]};
+	const proposalsFromBlockService = blockServiceData.multi_sign.proposal;
+	
 	let resultMessage = '';
 	let errorInfo = '';
 	let showNotification = false;
   	let link = '';
 
-	// placeholder for testing revoke button appearing for proposal submitted by this user
-	const w = $walletAddress
+	// get users wallet to check whether user has already confirmed a proposal
+	const w = $walletAddress;
 
-	let proposalFetch = [];
+	const proposalFetch = [];
 
-	// if (p.multi_sign.proposal){
-	// 	for (const i in p.multi_sign.proposal){
-	// 		proposalFetch.push({
-	// 			id:i, 
-	// 			type:p.multi_sign.proposal[i].type || p.type, 
-	// 			proposal:JSON.stringify(p.multi_sign.proposal[i]) || p.proposal,
-	// 			//confirmNum: c.multi_sign.owner_confirmed[i].length || confirmNum,
-	// 			confirmNum: c.value.length ,
-	// 			iSubmitted: c.value.includes(w)
-	// 			//iSubmitted: c.multi_sign.owner_confirmed[i].includes(w)
-	// 		})
-	// 	}
-	// };
-
-	if ( p.proposal){
-		for (const i in p.proposal){
+	if (proposalsFromBlockService){
+		for (const i in proposalsFromBlockService){
 			proposalFetch.push({
 				id:i, 
-				type: p.proposal[i].type, 
-				proposal:JSON.stringify(p.proposal[i]),
+				type: proposalsFromBlockService[i].type, 
+				proposal:JSON.stringify(proposalsFromBlockService[i]),
 				//confirmNum: c.multi_sign.owner_confirmed[i].length || confirmNum,
-				confirmNum: c.value.length ,
-				iSubmitted: c.value.includes(w)
-				//iSubmitted: c.multi_sign.owner_confirmed[i].includes(w)
+				confirmNum: blockServiceData.multi_sign.owner_confirmed[i].length ,
+				iSubmitted: blockServiceData.multi_sign.owner_confirmed[i].includes(w)
 			})
 		}
 	};
@@ -96,9 +73,6 @@
 		}
 	}
 
-	//console.log(`proposalsPage: ${c.value.length}`)
-	// refecth confirmNum data and remove pending proposal when executed
-
 </script>
 
 <div class='head-bar'>
@@ -107,18 +81,10 @@
     <p>Actions</p>
 </div>
 
-{#if p}
 
-	<!-- {#each proposalFetch as propsl}
-		<ProposalList 
-			on:message = {forSlidingMenu}
-			proposalId={propsl.id} 
-			proposalType={propsl.type} 
-			proposalSubmitted={propsl.proposal}
-			confirmNum={propsl.confirmNum}
-			confirmed={propsl.iSubmitted}
-		/>
-	{/each} -->
+<!--display only if there are proposals-->
+{#if proposalsFromBlockService}
+
 	<ProposalList proposals={proposalFetch}/>
 
 {:else}
