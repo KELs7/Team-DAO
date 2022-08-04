7e273414f99c4f090a2c0c42ace202cfb2cec98d
kels7 <hiketabo@gmail.com>
2022-08-04 03:12:32 +0000

    default  to adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,12 @@
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		adapter: adapter()
-// 	}
-// };
-
-// export default config;
-
-import adapter from '@sveltejs/adapter-netlify';
-
-export default {
-  kit: {
-    // default options are shown
-    adapter: adapter({
-		edge: true
-	})
-  }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
+	}
 };
+
+export default config;
+
+
