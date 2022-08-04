9d7133c1b297ca4111a0ab22fa9a13c5dcc54e52
kels7 <hiketabo@gmail.com>
2022-08-04 02:07:20 +0000

    make it work with mock data instead of actual block service

--- src/routes/pendingProposals.svelte
+++ src/routes/pendingProposals.svelte
@@ -1,4 +1,4 @@
-<script context='module'>
+<!-- <script context='module'>
 	export async function load({fetch}){
         const res = await fetch('http://localhost:3535/current/all/multi_sign/');
         const blockServiceData = await res.json();
@@ -9,14 +9,13 @@
         }
     }
 		
-</script> 
+</script>  -->
 
 <script>
 	import ProposalList from '$lib/proposalSomthing.svelte';
 	import { walletAddress } from '$lib/stores/controllerStore.js';
-    //import { Socket } from 'dgram';
+	import blockServiceData from'$lib/config/mockDataForPendingPage.json';
 
-	//import { io } from 'node_modules/socket.io/client-dist/socket.io.js';
 	// import { io } from 'socket.io-client';
 
 	// const socket = io("ws://localhost:3535");
@@ -27,7 +26,7 @@
 	// 	console.log(msg);
 	// })
 
-	export let blockServiceData;
+	//export let blockServiceData;
 	const noData =false;
 
 	const proposalsFromBlockService = blockServiceData.multi_sign.proposal;
@@ -52,28 +51,15 @@
 	};
 
 
-	// let n = 0;
-	// const forSlidingMenu = (event)=> {
-	// 	n++;
-	// 	resultMessage = event.detail.resultMessage;
-	// 	errorInfo = event.detail.errorInfo;
-	// 	showNotification = event.detail.notification;
-	// 	link = event.detail.link;
-		
-	// 	if (n==2){
-	// 		setTimeout(()=>{
-	// 			location.reload(); 
-	// 			n = 0;	
-	// 		}, 3200)
-	// 	}
-	// }
+
 
 </script>
 
 <div class='head-bar'>
-	<p >Id</p>
-	<p style="justify-self: start">Type</p>
-    <p>Actions</p>
+	<div style="grid-column: 1/2; margin-left: 0.8rem">Id</div>
+	<!--p style="justify-self: start">Type</p-->
+	<div id="proposal-type" style="grid-column: 2/3">Type</div>
+    <div id="proposal-buttons" style="grid-column: 3/5">Actions</div>
 </div>
 
 
@@ -88,25 +74,22 @@
 
 <style>
     .head-bar {
-		display: flex;
-		/* grid-template-columns: repeat(3,1fr); */
-		justify-content:space-around;
-		align-content: center;
-		/* transition: all 500ms ease;  */
-		background-color: var(--panel-background-color);
-		border-radius: 8px 8px 0 0;
-		position: relative;
-		width: 81%;
+		display:grid; 
+		grid-template-columns: 1fr 1fr 2fr;
+		align-items: center;
+		background-color: var(--panel-background-color); 
+		border-radius: 8px 8px 0 0; 
+		width: 80%;
+		margin: auto; 
+		font-size: 0.7rem; 
 		height: 2rem;
-		margin: auto;
 		margin-top: 1rem;
-		font-size: 0.8rem;
 		user-select: none;
 		cursor: pointer;
 	}
-	.head-bar p {
+	/* .head-bar div {
 		margin-right: 3.2rem;
-	}
+	} */
 	.no-pending {
 		color: grey;
 		position: relative;
@@ -119,14 +102,13 @@
 		user-select: none;
 		cursor: pointer;
 	}
-	@media (min-width: 1022px){
-        .head-bar p {
-		margin-right: 4.2rem;
-		}
-    }
+	
 	@media (max-width: 480px) {
-		.head-bar p {
-			margin-right: 2.4rem;
+		.head-bar #proposal-type {
+			text-align:center;
+		}
+		.head-bar #proposal-buttons {
+			text-align:center;
 		}
     }
 </style>
