319e187f5eb20a6d48df9fa6e39ff9640bd0948d
kels7 <hiketabo@gmail.com>
2022-08-19 01:00:23 +0000

    make custom error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,29 @@
+<script context="module">
+	
+	export function load({ error}) {
+		return {
+			props: { message: error.message}
+		};
+	}
+</script>
+
+<script>
+	
+	export let message;
+
+</script>
+
+<div class="err">{message}</div>
+
+<style>
+    .err {
+		font-size: 0.7em;
+		color: red;
+       	text-align: center;
+        margin: auto;
+		width: 80%;
+		box-sizing: border-box;
+		/* outline: 0.8px solid whitesmoke; */
+		overflow: scroll; 
+    }
+</style>
