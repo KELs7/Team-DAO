4ecb698783bcf43e2571f0340c9b25df019bd550
kels7 <hiketabo@gmail.com>
2022-08-24 14:42:25 +0000

    add closing button

--- src/lib/components/modal.svelte
+++ src/lib/components/modal.svelte
@@ -1,19 +1,21 @@
 <script>
     export let proposal;
+    export let showModal = false;
 </script>
+{#if showModal}
+    <div class="background">
 
-<div class="background">
-
-    <div class="container">
-        <p style="font-size: 1.2em;">Proposal</p>
-        <textarea rows="8">
-            
-            {JSON.stringify(proposal)}
-            
-        </textarea>
+        <div class="container">
+            <img src="icons/close-x.svg" alt="close modal" on:click={()=>showModal = false}/>
+            <p style="font-size: 1.2em;">Proposal</p>
+            <textarea rows="8" readonly>
+                
+                {JSON.stringify(proposal)}
+                
+            </textarea>
+        </div>
     </div>
-</div>
-
+{/if}
 
 <style>
     .background {
@@ -29,7 +31,8 @@
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-        padding: 1em;
+        padding: 0.5em 1em 0.8em 1em;
+
         font-size: 0.8em;
         background-color: var(--panel-background-color);
         border-radius: 5px;
@@ -40,8 +43,16 @@
     textarea {
         width: 98%; 
         background-color: #000000; 
+        outline-style: none;
         color: var(--primary-color-dim);
         font-size: 0.8em;
         border-style: none;
+        border-radius: 5px;
+        
+    }
+    img {
+        position: absolute;
+        top: 0;
+        right: 0;
     }
 </style>
