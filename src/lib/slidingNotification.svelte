c8d45af8a09f74536a403edb63da7e408f6e3399
kels7 <hiketabo@gmail.com>
2022-08-31 11:36:15 +0000

    change toaster display and operation

--- src/lib/slidingNotification.svelte
+++ src/lib/slidingNotification.svelte
@@ -8,29 +8,32 @@
 </script>
 
 {#if show}
-    <div transition:fly='{{y:20, duration: 600}}' class='container'>
-	{#if link}
-		<p style>sucess!</p>
-	{#if resultMessage}
-		<p class='result-container'>{resultMessage}</p>
-	{/if}
-		<a href={link}>view transaction</a>
-	{/if}
-	{#if errorInfo}
-		<p style='padding: 0.8em 0; text-align: center; width:80%; overflow-x:scroll; color: red; border: 1px solid white'>{errorInfo}</p>
-	{/if}
+    <!--div transition:fly='{{y:20, duration: 600}}' class='container'-->
+	<div transition:fly='{{x:100, duration: 600}}' class="container {errorInfo?'error-toast':''}">
+		{#if link}
+			<p>sucess!</p>
+		{/if}
+		{#if resultMessage}
+			<p class='result-container'>{resultMessage}</p>
+		{/if}
+		{#if link}
+			<a style='color: var(--flat-secondary)' href={link}>view transaction</a>
+		{/if}
+		{#if errorInfo}
+			<p class = "err">{errorInfo}</p>
+		{/if}
     </div>
 {/if}
 
 <style>
-	.container {
+	/* .container {
 		display: flex;
 		z-index: 100;
 		padding: 0.5em 0 0.8em 0;
-    	flex-direction: column;
+    	
 		justify-content: center;
 		align-items: center;
-    	font-size: 0.68em;
+    	font-size: 0.7em;
 		color: #000000;
 		position: fixed;
 		height: 15%;
@@ -39,12 +42,51 @@
 		bottom: 0;
 		background-color: #24ffbd;
 		line-height: 0.5em;
-	}
+	} */
+	.container {
+        display: flex;
+		flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        outline: 0.1px solid grey;
+        background-color: var(--panel-background-color);
+		margin-top: 8em;
+        font-size: 0.68em;
+        position: fixed; 
+        z-index: 99;
+        border-left: 7px solid var(--flat-primary);
+        border-radius: 2px;
+        height: 7em;
+        width: 14em;
+        right: 0.7em;    
+    }
 	.result-container{
 		margin-top: -0.2em; 
 		font-size: 0.68em;
 		width: 80%;
 		overflow: hidden;
+	}
+	.err {
+		padding: 0.8em 0; 
+		text-align: center; 
+		width:80%; 
+		overflow-x:scroll; 
+		color: red; 
+		/* border: 1px solid white */
+	}
+	.err::-webkit-scrollbar{
+        height: 2px;
+    }   
+    .err::-webkit-scrollbar-thumb{
+        background-color: var(--flat-primary);
+        border-radius: 5px;
+    }
+    .err::-webkit-scrollbar-corner{
+        display: none;
+    }
+	.error-toast {
+
+		border-left: 10px solid red;
 	}
 	
 </style>
