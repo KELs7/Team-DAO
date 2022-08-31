535241f0a3b59493ed91d2f4b75a1516f48fabaf
kels7 <hiketabo@gmail.com>
2022-08-31 11:35:09 +0000

    remove return message

--- src/lib/stores/toasterInfo.js
+++ src/lib/stores/toasterInfo.js
@@ -1,6 +1,6 @@
 import { writable} from 'svelte/store';
 
-export let resultMessage = writable('')
+//export let resultMessage = writable('{"abc":"abchshs", "cdf": "12334"}')
 export let errorInfo = writable('')
 export let link = writable('')
 export let showNotification = writable(false)
