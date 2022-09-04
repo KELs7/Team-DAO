c7ee5c83704ac979e0a16b2786d8b9ef2a97a6a4
kels7 <hiketabo@gmail.com>
2022-09-04 23:38:39 +0000

    remove if condition, lwc is first stored by menu component and cleanup

--- src/lib/proposalSomthing.svelte
+++ src/lib/proposalSomthing.svelte
@@ -1,7 +1,7 @@
 <script>
     import { onMount } from 'svelte';
     import { lwc } from '$lib/stores/controllerStore.js';
-    import { resultMessage, errorInfo, link, showNotification } from '$lib/stores/toasterInfo.js';
+    import { errorInfo, link, showNotification } from '$lib/stores/toasterInfo.js';
     import { confirmProposal, revokeProposal} from '../js/funcs.js'
     import { proposalsStore } from '$lib/stores/proposals.js';
     import Modal from './components/modal.svelte';
@@ -67,9 +67,9 @@
 
     //lamdenWalletController listener here
     onMount(()=>{
-        if ($lwc){
-            $lwc.events.on('txStatus', handleTxResults)
-        }
+        
+        $lwc.events.on('txStatus', handleTxResults)
+        
     })
 
     const confirm = (e)=>{
@@ -86,73 +86,10 @@
         $lwc.sendTransaction(propslInfo);
     }
 
-    //ptype , action-btn
-
 </script>
 
 <div class='container'>
-
-    <!-- <ul class="inside-container">
-
-        {#each proposals as proposal}
-
-        <li>
-            <div style="display: flex; justify-content:space-around; align-content: center;">
-            <div style="display:grid; grid-template-columns: 1fr 1fr 2fr">
-                <div style="grid-column: 1/2; margin-left: 0.88em; outline: 2px solid blue">{proposal.id}</div>
-                <div style="grid-column: 2/3; outline: 2px solid blue">{proposal.type}</div>
-                <div style="grid-column: 3/5; outline: 2px solid blue">
-
-                    <button 
-                        class="outlined primary white"
-                        id = {proposal.id}
-                        on:click={()=>{
-                            view==proposal.id?viewStateBtn='view':viewStateBtn='close';
-                            if(view==proposal.id){
-                                view=0;
-                            }else{
-                                view=proposal.id;
-                            }
-                        }}
-                    >
-                        <div><strong>{viewStateBtn}</strong></div>
-                    </button>
-
-                                       
-                    <button 
-                        class="outlined primary white"
-                        
-                        on:click|once={confirm}
-                        bind:this={confirmBtns[proposal.id]}
-                        ><div><strong id = {proposal.id}>{
-                            confirmBtnLabel[proposal.id]?confirmBtnLabel[proposal.id]:`confirm ${proposal.confirmNum}`
-                        }</strong></div>
-                    </button>
-                    
-                    {#if true}
-                        <button 
-                            class="outlined primary white"
-                            
-                            on:click|once={revoke}
-                            bind:this={revokeBtns[proposal.id]}
-                        ><div><strong id = {proposal.id}>{
-                            revokeBtnLabel[proposal.id]?revokeBtnLabel[proposal.id]:'revoke'
-                            }</strong></div>
-                        </button>
-                    
-                    {/if} 
-                </div>        
-            </div> 
-        </li>
-
-        {#if view == proposal.id}
-            <div transition:fly='{{y:-5, duration: 600}}' class='proposal-view'>
-                {proposal.proposal}
-            </div>
-        {/if}
-        {/each}
-    </ul> -->
-
+    
     <table >
         <thead style="font-size: 0.8em; border-bottom: 2px solid black">
             <tr>
@@ -237,48 +174,9 @@
     .container::-webkit-scrollbar-corner{
         display: none;
     }
-    .inside-container {
-        display: flex;
-        flex-direction: column;
-        justify-content:space-around;
-        align-content: center; 
-        width: 100%;
-        box-sizing: border-box;
-        margin: auto;
-        font-size: 0.7em;
-        color: white;
-        background-color: var(--panel-background-color);
-        padding: 0.98em 0 0 0;
-    }
-    .proposal-view{
-        width: 100%;
-        margin: 0;
-        padding: 2em 1em;
-        /*margin-top: 0.5rem;*/
-        background-color: #000000;
-        font-family: 'Segoe UI';
-        font-size: 1em;
-        color: var(--flat-primary);
-        border: 0.125em solid var(--flat-primary);
-        box-sizing: border-box;
-        overflow:scroll;
-
-    }
     button {
         font-size: 0.77em;
         margin-right: 1em;
-    }
-    li {
-        /* display: inline-block; */
-        list-style: none;
-        padding: 0.88em 0 0.88em 0;
-        border-bottom: 0.0625em solid #000000;
-    }
-    @media (max-width: 480px) {
-        .inside-container{
-            width: 160vw; 
-
-        }
     }
     @media (min-width: 623px) {
         table{
